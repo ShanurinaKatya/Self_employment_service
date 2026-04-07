b4661fb3303f3b3c747350c84306cfab6f46c0f5
Katya <shanurina.ekaterina@yandex.ru>
2026-04-07 21:01:01 +0300

    change file for product page

--- pages/product/Services_for_the_self-aware.js
+++ pages/product/Services_for_the_self-aware.js
@@ -11,7 +11,7 @@ export class ProductPage {
             1: {
                 title: "Регистрация в качестве самозанятого",
                 text: "Подача заявки в ФНС, присвоение статуса «плательщик НПД»",
-                src: "https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg",
+                src: "https://cdn.gpb.ru/upload/files/iblock/f1c/h5pi4duvqhflhahp95loe4vrdshkptu7/x1_titul_2432x800.jpg",
                 result: "статус самозанятого, можно выдавать чеки",
                 term: "1–3 дня",
                 price: "бесплатно",
@@ -20,7 +20,7 @@ export class ProductPage {
             2: {
                 title: "Изменение вида деятельности",
                 text: "Добавление, удаление или замена кодов ОКВЭД/ОКПДТ",
-                src: "https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg",
+                src: "https://cdn.gpb.ru/upload/files/iblock/18e/674mzgnqv20xyt6ryxad0t2trazpd3wz/x1_inside_2432x800.jpg",
                 result: "новые коды видов деятельности в реестре",
                 term: "1 день",
                 price: "бесплатно",
@@ -29,7 +29,7 @@ export class ProductPage {
             3: {
                 title: "Снятие с учёта самозанятого",
                 text: "Прекращение деятельности в режиме НПД",
-                src: "https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg",
+                src: "https://cdn.gpb.ru/upload/files/iblock/bf2/x53am725mw4hcw3fbvjeqw1h3ypzmmnw/titul_1200x630-_-2024_12_02T124856.085.png",
                 result: "вы больше не самозанятый, налог платить не надо",
                 term: "до 2 дней",
                 price: "бесплатно",
@@ -38,7 +38,7 @@ export class ProductPage {
             4: {
                 title: "Выдача справки о постановке на учёт",
                 text: "Официальный документ из ФНС для банков и заказчиков",
-                src: "https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg",
+                src: "https://cdn.gpb.ru/upload/files/iblock/b78/9fdyo75ph27322mcx3kla766p54vsgj2/x1_IP.png",
                 result: "официальная справка из ФНС (файл PDF)",
                 term: "1 день",
                 price: "бесплатно",
@@ -47,7 +47,7 @@ export class ProductPage {
             5: {
                 title: "Помощь в уплате налога",
                 text: "Проверка начислений, формирование квитанции, контроль оплаты",
-                src: "https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg",
+                src: "https://cdn.gpb.ru/upload/files/iblock/107/skbum8ndum12z2s9s6c9qsyngd6jc5do/x1_titul_2432x800.jpg",
                 result: "оплаченный налог, квитанция, уведомление",
                 term: "сразу онлайн",
                 price: "бесплатно (оплачиваете только сам налог)",

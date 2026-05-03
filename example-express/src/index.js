02846a057fa259d87fdb5605d58e1cbed8f3bef2
Katya <shanurina.ekaterina@yandex.ru>
2026-05-03 23:38:48 +0300

    Update lab for lab6

--- example-express/src/index.js
+++ example-express/src/index.js
@@ -17,24 +17,36 @@ app.use(express.json());
 
 // Логгер
 app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    console.log('[' + new Date().toISOString() + '] ' + req.method + ' ' + req.url);
     next();
 });
 
 // Подключаем маршруты по новому пути
 app.use('/self-employment-services', servicesRouter);
 
+// Раздача статики из папки public (аналог app.useStaticAssets в NestJS)
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Перенаправление всех GET-запросов на index.html (SPA)
+app.use(function(req, res, next) {
+    if (req.method === 'GET') {
+        res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+    } else {
+        next();
+    }
+});
+
 // 404
-app.use((req, res) => {
+app.use(function(req, res) {
     res.status(404).json({ error: 'Маршрут не найден' });
 });
 
 // Глобальный error handler
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
     console.error(err);
     res.status(500).json({ error: 'Внутренняя ошибка сервера' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Сервер запущен на http://localhost:${PORT}`);
+app.listen(PORT, function() {
+    console.log('Сервер запущен на http://localhost:' + PORT);
 });
